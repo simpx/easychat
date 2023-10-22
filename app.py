39a9f90f431c450e21534fd4c44ba91d43266b43
simpx <simpxx@gmail.com>
2023-10-22 14:42:16 +0000

    add gpt

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import xml.etree.cElementTree as ET
 import yaml
 import logging
 import json
+import openai
 from weworkapi_python.callback.WXBizMsgCrypt3 import WXBizMsgCrypt
 from easy_wework import get_messages, send_text, load_config
 
@@ -18,8 +19,10 @@ with open('config.yaml', 'r') as f:
     sEncodingAESKey = config['encoding_aes_key']
     sCorpID = config['corpid']
     sSecret = config['secret']
+    sApikey = config['api_key']
 
 load_config('config.yaml')
+openai.api_key = sApikey
 
 wxcpt = WXBizMsgCrypt(sToken, sEncodingAESKey, sCorpID)
 
@@ -36,8 +39,17 @@ def verify_url():
         return "ERROR", 403
     return sEchoStr
 
+def ask_gpt(txt):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "system", "content": "你是一个专业、精准、简洁的助手"},
+                  {"role": "user", "content": f"{txt}\n"}],
+        max_tokens=500,
+        temperature=0.7,
+    )
+    return response['choices'][0]['message']['content']
 
-cursor = None
+cursor = "4gw7MepFLfgF2VC5nph"
 @app.route('/verify_url', methods=['POST'])
 def weixin():
     global cursor
@@ -63,12 +75,16 @@ def weixin():
 
     messages = get_messages(req['Token'], cursor)
     cursor = messages['next_cursor']
+    logging.info(f"cursor is: {cursor}")
     for message in messages['msg_list']:
         try:
-            send_text(message['external_userid'], message['open_kfid'], message['text']['content'])
+            answer = ask_gpt(message['text']['content'])
+            send_text(message['external_userid'], message['open_kfid'], answer)
+            message_str = json.dumps(message, indent=4)
+            logging.info(f"Success in : {message_str}")
         except Exception as e:
             message_str = json.dumps(message, indent=4)
-            logging.error(f"Failed in send_text: {message_str}")
+            logging.exception(f"Failed in send_text: {message_str}")
             
     return ''
 
