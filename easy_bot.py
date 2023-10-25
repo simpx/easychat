79ea070f4315f5720f2599137971fc29440fb2e2
simpx <simpxx@gmail.com>
2023-10-25 16:32:12 +0000

    seems work

--- easy_bot.py
+++ easy_bot.py
@@ -1,32 +1,44 @@
 import re
+import yaml
+import json
 from typing import Callable, List
 from flask import Flask, request, jsonify
+import logging
+from easy_wework import get_messages, send_text, load_config, send_menu
+import easy_wework
+
+load_config('config.yaml')
+logging.basicConfig(level=logging.DEBUG)
 
 class Session:
-    def __init__(self, user_id):
+    def __init__(self, user_id, bot_id):
         self.user_id = user_id
-        self.chat = []
+        self.bot_id = bot_id
+        self.messages = []
 
     def send_message(self, message):
-        # 这里只会更新session记录，并没有真正的发送机制，实际上可能需要接入微信API进行消息发送。
-        self.chat.append({"bot": "SYSTEM", "message": message})
+        self.messages.append({"role": "bot", "id": self.bot_id, "content": message})
 
-class EasyWeWork:
-    def __init__(self):
+class EasyBot:
+    def __init__(self, url, cursor = None):
         self.app = Flask("easy_wework")
         self.sessions = {}
         self.chat_callbacks = []
         self.event_callbacks = []
+        self.url = url
+        self.cursor = cursor
         self._setup_routes()
 
     def _setup_routes(self):
-        @self.app.route("/chat", methods=["POST"])
+        @self.app.route(self.url, methods=["POST"])
         def chat_route():
             return self._handle_chat()
 
-        @self.app.route("/event", methods=["POST"])
+        '''
+        @self.app.route(, methods=["POST"])
         def event_route():
             return self._handle_event()
+        '''
 
     def on_chat(self, bot_ids: List[str]):
         def decorator(callback: Callable):
@@ -34,11 +46,13 @@ class EasyWeWork:
             return callback
         return decorator
 
+    '''
     def on_event(self, bot_ids: List[str]):
         def decorator(callback: Callable):
             self.event_callbacks.append((bot_ids, callback))
             return callback
         return decorator
+    '''
 
     def run(self, host="127.0.0.1", port=5000):
         self.app.run(host=host, port=port)
@@ -46,41 +60,45 @@ class EasyWeWork:
     def _get_callback(self, bot_id, callback_list):
         for ids, callback in callback_list:
             for id_pattern in ids:
+                logging.info(f"id_pattern {id_pattern}, bot_id {bot_id}")
                 if re.match(id_pattern, bot_id) or id_pattern == "*":
                     return callback
         return None
 
     def _handle_chat(self):
-        user_id = request.json["user_id"]
-        message = request.json["message"]
-        bot_id = request.json["bot_id"]
-
-        if user_id not in self.sessions:
-            self.sessions[user_id] = Session(user_id)
-
-        self.sessions[user_id].chat.append({"user": user_id, "message": message})
-
-        callback = self._get_callback(bot_id, self.chat_callbacks)
-        if not callback:
-            return jsonify({"error": "No callback registered for this bot_id."})
-
-        response = callback(request, self.sessions[user_id])
-        if isinstance(response, str):
-            self.sessions[user_id].chat.append({"bot": bot_id, "message": response})
-            return jsonify({"bot": bot_id, "message": response})
-
-    def _handle_event(self):
-        event_type = request.json["type"]
-        user_id = request.json["user_id"]
-        bot_id = request.json["bot_id"]
-
-        if user_id not in self.sessions:
-            self.sessions[user_id] = Session(user_id)
-
-        callback = self._get_callback(bot_id, self.event_callbacks)
-        if not callback:
-            return jsonify({"error": "No callback registered for this bot_id."})
-
-        callback(request, self.sessions[user_id])
-        return jsonify({"status": "OK"})
-
+        sReqMsgSig = request.args.get('msg_signature')
+        sReqTimeStamp = request.args.get('timestamp')
+        sReqNonce = request.args.get('nonce')
+        sReqData = request.data
+        ret, req = easy_wework.decrypt_msg(sReqData, sReqMsgSig, sReqTimeStamp, sReqNonce)
+        if ret != 0:
+            logging.error(f"Failed in DecryptMsg, error code: {ret}")
+            return "ERROR", 403
+        messages = get_messages(req['Token'], cursor = self.cursor)
+        self.cursor = messages['next_cursor']
+        logging.info(f"cursor is: {self.cursor}")
+        for message in messages['msg_list']:
+            if message['msgtype'] != 'text' or "menu_id" in message['text']:
+                message_str = json.dumps(message, indent=4)
+                logging.info(f"Skip message : {message_str}")
+                continue
+            try:
+                user_id = message['external_userid']
+                user_msg = message['text']['content']
+                bot_id = message['open_kfid']
+                message_str = json.dumps(user_msg, indent=4)
+                if user_id not in self.sessions:
+                    self.sessions[user_id] = Session(user_id, bot_id)
+                self.sessions[user_id].messages.append({"role": "user", "id": user_id, "content": message})
+                callback = self._get_callback(bot_id, self.chat_callbacks)
+                if not callback:
+                    return "ERROR", 500
+                r = {"type": "message", "user_id": user_id, "bot_id": bot_id, "content": user_msg}
+                response = callback(r, self.sessions[user_id])
+                if isinstance(response, str):
+                    self.sessions[user_id].messages.append({"role": "bot", "id": bot_id, "content": response})
+                logging.info(f"Success in : {message_str}")
+            except Exception as e:
+                message_str = json.dumps(message, indent=4)
+                send_text(message['external_userid'], message['open_kfid'], "出错了，请重新提问")
+                logging.exception(f"Failed in send_text: {message_str}")
