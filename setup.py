ea37cd70721f6c01328a91e216ce7ee0e49bf366
simpx <simpxx@gmail.com>
2023-10-26 15:39:59 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='easychat',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/simpx/easychat',
+    license='MIT',
+    author='simpx',
+    author_email='simpxx@gmail.com',
+    description='A simple chatbot framework',
+    install_requires=[
+        'Flask', 
+		'pycryptodome',
+		'PyYAML'
+    ],
+    classifiers=[
+        # 选择合适的分类器
+        'Development Status :: 3 - Alpha',  # 或其他状态
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
